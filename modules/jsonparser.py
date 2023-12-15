4903fcc7d26a713e1d6d781e95ee7a6eb19bc3ed
nimaaskarian <nimaas1400@gmail.com>
2023-12-15 18:58:06 +0330

    Added jsonparser module

---
+++ modules/jsonparser.py
@@ -0,0 +1,18 @@
+import json
+from typing import List
+
+class Documents:
+    query:str
+    candidates: List[int]
+    selected: int
+    def __init__(self, query, candidates, selected) -> None:
+        self.query = query
+        self.candidates = candidates
+        self.selected = selected
+
+def json_docs(path)->List:
+    with open(path) as file:
+        data = json.load(file)
+
+    return [Documents(document["query"], document["candidate_documents_id"], document["is_selected"].index(1)) 
+              for document in data]
